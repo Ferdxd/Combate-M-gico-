a46050a669a30050c136ed17f61d4f95086dd46b
Ferdy Agustín <ferdyomar12@gmail.com>
2024-02-05 20:12:29 -0600

    se empezo a agregar un prototipo de logica de combate al juego

--- js/combate-magico.js
+++ js/combate-magico.js
@@ -8,11 +8,27 @@ let rbtn_hermione
 let rbtn_harry
 let rbtn_ron
 let personajes=[]
+let personajeJugador
+let personajeEnemigo
+let vidaEnemigo
+let vidaJugador
+
 
 //section personajes
 const divContenedorPersonajes=document.getElementById('contenedorPersonajes')
-
+const divContenedorAtaques=document.getElementById('contenedorAtaques')
+const divContenedorDefensas=document.getElementById('contenedorDefensas')
 const personaje_Seleccionado=document.getElementById('btn_personaje')
+
+
+//section modo-combate
+
+const vidasJugador=document.getElementById('vidas-jugador')
+const nombreJugador=document.getElementById('nombre-jugador')
+
+const vidasEnemigo=document.getElementById('vidas-enemigo')
+const nombreEnemigo=document.getElementById('nombre-enemigo')
+
 
 //estructura de cada personaje
 class Personaje{
@@ -36,18 +52,28 @@ class Personaje{
 
 //creacion de personajes jugables
 
-let harryPotter=new Personaje('Harry Potter', './assets/harry_potter.png',5,'harry')
-let ronWeasley=new Personaje('Ron Weasley','./assets/Ron_Weasley.png',5,'ron')
-let hermioneGranger=new Personaje('Hermione Granger','./assets/Hermione_Granger.png',5,'hermione')
+let harryPotter=new Personaje('Harry Potter', './assets/harry_potter.png',10,'harry')
+let ronWeasley=new Personaje('Ron Weasley','./assets/Ron_Weasley.png',10,'ron')
+let hermioneGranger=new Personaje('Hermione Granger','./assets/Hermione_Granger.png',10,'hermione')
 
 //ataques disponibles
 // <p>Expelliarmus, Stupefy, Reducto</p>
 // <p>Protego, Escudo Magico</p>
 
+//botones ataques
+let btnExpelliarmus
+let btnStupefy
+let btnReducto
+let botonesAtaques=[]
+let secuenciaAtaquesJugador=[]
+//botones Defensas
+let btnProtego
+let btnEscudoMagico
+
 const ataquesTipoDaño=[
-    {nombre:'Expelliarmus', descripcion:'Desarma al oponente y causa daño moderado'},
-    {nombre:'Stupefy',descripcion:'Aturde al oponente y causa daño leve'},
-    {nombre:'Reducto',descripcion:'Causa daño significativo al oponente'}
+    {nombre:'Expelliarmus', descripcion:'Desarma al oponente y causa daño moderado', damage:3},
+    {nombre:'Stupefy',descripcion:'Aturde al oponente y causa daño leve', damage:2},
+    {nombre:'Reducto',descripcion:'Causa daño significativo al oponente', damage:4}
 ]
 
 const hechizosDefensa=[
@@ -99,20 +125,161 @@ function modo_multijugador(){
 }
 
 function seleccionPersonaje(){
-    let nombre_personaje
     if(rbtn_harry.checked){
-        nombre_personaje=rbtn_harry.id
+        personajeJugador=seleccionPersonajeJugador(rbtn_harry.id)
     }else if(rbtn_hermione.checked){
-        nombre_personaje=rbtn_hermione.id
+        personajeJugador=seleccionPersonajeJugador(rbtn_hermione.id)
     }else if(rbtn_ron.checked){
-        nombre_personaje=rbtn_ron.id
+        personajeJugador=seleccionPersonajeJugador(rbtn_ron.id)
     }else{
         alert("No has seleccionado ningun personaje")
         return
     }
 
-    alert("El personaje que seleccionaste es: "+nombre_personaje)
 
+    
+    nombreJugador.innerHTML=personajeJugador.nombre
+    vidaJugador=personajeJugador.vida
+
+    alert("El personaje que seleccionaste es: "+personajeJugador.nombre)
+
+    seleccionEnemigo()
+
+    vidaEnemigo=personajeEnemigo.vida
+    actualizarVida()
+    extraerAtaques(personajeJugador)
+    //console.log(extraerAtaques(personajeJugador))
+
+}
+
+
+function seleccionPersonajeJugador(id){
+    let apuntador
+    personajes.forEach((personaje)=>{
+        if(personaje.id==id){
+            apuntador=personaje
+        }
+    })
+    return apuntador
+}
+
+
+
+function seleccionEnemigo(){
+    let personajeElegido=aleatorio(0,personajes.length-1)
+    personajeEnemigo=personajes[personajeElegido]
+   console.log(personajeEnemigo.nombre)
+   nombreEnemigo.innerHTML= personajeEnemigo.nombre
+   
+}
+
+
+function extraerAtaques(personajeJugador){
+    let ataques=personajeJugador.ataques
+    let defensas=personajeJugador.defensas
+    
+    console.log(ataques)
+    console.log(defensas)
+    mostrarSkills(ataques,'boton-ataques',divContenedorAtaques)
+        
+    btnExpelliarmus=document.getElementById('Expelliarmus')
+    btnStupefy=document.getElementById('Stupefy')
+    btnReducto=document.getElementById('Reducto')
+    botonesAtaques=document.querySelectorAll('.boton-ataques')
+    mostrarSkills(defensas,'boton-defensas',divContenedorDefensas)
+
+    secuenciaCombate()
+
+//botones Defensas
+    //btnProtego
+    //btnEscudoMagico
+}
+
+
+function secuenciaCombate(){
+    botonesAtaques.forEach((boton)=>{
+        boton.addEventListener('click',(e)=>{
+            if(e.target.textContent=='Expelliarmus'){
+                secuenciaAtaquesJugador.push('Expelliarmus')
+                //console.log(secuenciaAtaquesJugador)
+                lanzarAtaque('Expelliarmus')
+                lanzarAtaqueEnemigo()
+                boton.style.background='white'
+                boton.disabled=true
+            }else if(e.target.textContent=='Stupefy'){
+                secuenciaAtaquesJugador.push('Stupefy')
+                lanzarAtaque('Stupefy')
+                lanzarAtaqueEnemigo()
+                boton.style.background='white'
+                boton.disabled=true
+            }else if(e.target.textContent=='Reducto'){
+                secuenciaAtaquesJugador.push('Reducto')
+                lanzarAtaque('Reducto')
+
+                lanzarAtaqueEnemigo()
+                boton.style.background='white'
+                boton.disabled=true
+            }
+            if(secuenciaAtaquesJugador.length==3){
+                alert("ya has presionado todos los botones")
+            }
+        })
+    })
+    
+}
+
+function actualizarVida(){
+    vidasEnemigo.innerHTML="vida "+vidaEnemigo
+    vidasJugador.innerHTML="vida "+vidaJugador
+}
+
+function lanzarAtaque(nombreAtaque){
+    if('Expelliarmus'==nombreAtaque){
+        vidaEnemigo-=3
+        //alert(personajeJugador.nombre+ "Lanza Hechizo "+" a "+personajeEnemigo.nombre)
+        actualizarVida()
+    }else if('Stupefy'==nombreAtaque){
+        vidaEnemigo-=2
+        actualizarVida()
+    }else{
+        vidaEnemigo-=4
+        actualizarVida()
+    }
+
+}
+function lanzarAtaqueEnemigo(){
+    
+    let atack=ataquesTipoDaño[aleatorio(0,ataquesTipoDaño.length-1)].nombre
+    console.log(atack)
+    if('Expelliarmus'==atack){
+        vidaJugador-=3
+        //alert(personajeJugador.nombre+ "Lanza Hechizo "+" a "+personajeEnemigo.nombre)
+        actualizarVida()
+    }else if('Stupefy'==atack){
+        vidaJugador-=2
+        actualizarVida()
+    }else{
+        vidaJugador-=4
+        actualizarVida()
+    }
+}
+
+
+
+
+function mostrarSkills(skills,claseboton,divs){
+    
+    skills.forEach((skill)=>{
+        divs.innerHTML+=`<button id=${skill.nombre} class=${claseboton}>${skill.nombre}</button>`
+    })
+}
+
+
+
+
+//funciones extra
+function aleatorio(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min)
 }
 
 window.addEventListener('load',main)
